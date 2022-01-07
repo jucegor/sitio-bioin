0feb743df767b84c0ae3850781580aec8c84d6ce
Julio Gonzalez <jucegor@gmail.com>
2022-01-07 13:24:36 -0600

    Navbar finished, still not responsive

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,34 +1,16 @@
 export function navbar() {
-  // ---------horizontal-navbar-menu-----------
-  var tabsNewAnim = document.querySelector('#navbar-animmenu');
-  var selectorNewAnim = document.querySelector('#navbar-animmenu');
-  var optionsOnNavbar = document.getElementById('navbar-animmenu').getElementsByTagName('li');
-  var listItemsLenght = optionsOnNavbar.length;
-  var activeItemNewAnim = document.getElementsByClassName('active-li').item(0);
-  var activeWidthNewAnimWidth = activeItemNewAnim.clientWidth;
-  var itemPosNewAnimLeft = activeItemNewAnim.offsetLeft;
+
+  var listItems = document.getElementById('navbar-animmenu').querySelectorAll('li');
 
   function unactiveItem() {
-    activeItemNewAnim.classList.remove('active-li');
+    listItems.forEach(item => item.classList.remove('active-li'));
   }
 
-  function clicked() {
-    console.log("Success!!!");
-  }
-
-
-  console.log(optionsOnNavbar);
-  console.log(listItemsLenght);
-
-
-
-  function restyleItem() {
-    var item = document.getElementsByClassName('hori-selector');
-    item[0].setAttribute('style', `left: ${itemPosNewAnimLeft}px; width: ${activeWidthNewAnimWidth}px;`);
-  }
-  restyleItem();
-
-
-
-  console.log("Hello from navbar!");
+  listItems.forEach(item => item.onclick = function clicked(){
+    unactiveItem();
+    var activeWidthListItem = this.offsetWidth;
+    var activePosListItem = this.offsetLeft;
+    document.getElementsByClassName('hori-selector')[0].setAttribute('style', `left: ${activePosListItem}px; width: ${activeWidthListItem}px;`);
+    this.classList.add('active-li');
+  });
 }
