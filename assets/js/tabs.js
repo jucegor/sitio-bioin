658be0516934fa1c0f1431f130062988d18b5f42
Julio Gonzalez <jucegor@gmail.com>
2022-01-10 09:27:54 -0600

    start with tabs

---
+++ assets/js/tabs.js
@@ -0,0 +1,17 @@
+export function tabs() {
+  function showTab() {
+    // var i, tabContent;
+    // tabContent = document.getElementsByClassName('tabContent');
+    //   for (i = 0; i < tabContent.length; i++) {
+         console.log("success")
+
+    //   }
+  }
+
+  // var tabItems = document.getElementById('navbar-animmenu').querySelectorAll('li');
+
+  // tabItems.forEach(item => item.onclick = function clicked() {
+  //   showTab();
+  // });
+
+}
