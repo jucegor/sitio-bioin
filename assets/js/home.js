1e254e1108989bf2c3496a1c972e0584a230ccf9
Julio Gonzalez <jucegor@gmail.com>
2022-01-14 16:08:49 -0600

    Media queries fixed for smartphone

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,3 +1,20 @@
 export function home() {
-  console.log("hello from home");
+  function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+
+  window.addEventListener("scroll", reveal);
+
 }
